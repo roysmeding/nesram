9b2780c960e328b166025fbd3736e8b6d988ac31
Roy Smeding <roy.smeding@gmail.com>
2015-09-16 23:03:27 +0200

    Hierarchical clustering based on affinity propagation

--- cluster.py
+++ cluster.py
@@ -17,123 +17,45 @@ assert np.size(ent) == n_seqs
 
 sys.stderr.write("Number of datasets: %d\n" % n_seqs)
 
-# reduce
-THRESHOLD = 0.1
-mask = ent>THRESHOLD
-ent = ent[mask]
-A = A[mask,:][:,mask]
-
-n_seqs = np.size(A,0)
-sys.stderr.write("After reduction: %d\n" % n_seqs)
-
 # fix A
 A = A + np.transpose(A) - np.diag(A)
 
-# MDS
-sys.stderr.write("Reducing to 2D using MDS...\n")
-
-se = sklearn.manifold.MDS(dissimilarity='precomputed', n_jobs=-1)
-dimred = se.fit_transform(A)
-
-sys.stderr.write("\tStress = %f\n" % se.stress_)
-
 # construct affinity matrix
-aff = -A
+aff = np.exp(-A)
 
 sys.stderr.write("Clustering...\n")
 
 # cluster
-ap = sklearn.cluster.AffinityPropagation(affinity='precomputed')
-labels = ap.fit_predict(aff)
+edges = []
 
-WIDTH=3840
-HEIGHT=2160
+mapping = list(range(2048))
 
-print "Creating %dx%d image..." % (WIDTH, HEIGHT)
-surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
-ctx = cairo.Context (surface)
+while len(mapping) > 2:
+    ap = sklearn.cluster.AffinityPropagation(affinity='precomputed')
+    ap.fit(aff)
+    labels  = ap.labels_
+    indices = ap.cluster_centers_indices_
 
-xmin, xmax = np.min(dimred[:,0]), np.max(dimred[:,0])
-ymin, ymax = np.min(dimred[:,1]), np.max(dimred[:,1])
-w,h = xmax-xmin, ymax-ymin
+    for node,label in enumerate(labels):
+        if mapping[node] != mapping[indices[label]]:
+            edges.append((mapping[indices[label]], mapping[node]))
 
-ctx.move_to(   0.,     0.)
-ctx.line_to(   0., HEIGHT)
-ctx.line_to(WIDTH, HEIGHT)
-ctx.line_to(WIDTH,     0.)
-ctx.line_to(   0.,     0.)
-ctx.set_source_rgba(0.,0.,0.,1.)
-ctx.fill()
+    mapping = [mapping[k] for k in indices]
+    aff = aff[indices,:][:,indices]
 
-scale = min(WIDTH/w, HEIGHT/h) * 0.95
-ctx.scale(scale, scale)
+    sys.stderr.write("clustered to %d\n" % len(indices))
 
-tx,ty = (-xmin-xmax+WIDTH/scale)/2., (-ymin-ymax+HEIGHT/scale)/2.
-ctx.translate(tx, ty)
-
-cm = plt.cm.get_cmap('gist_rainbow')    # colormap for for nodes/edges based on index
-
-print "Drawing edges..."
-ctx.set_line_width(0.001)
-n = n_seqs*(n_seqs-1)/2
-
-l = len(str(n))
-fmt = '%'+str(l)+'d / %'+str(l)+'d'
-back = '\b' * (2*l+3)
-
-sys.stdout.write('\t' + (fmt % (0,n)))
-for j in range(1,n_seqs-1):
-    done = j*n_seqs - j*(j+1)/2
-    sys.stdout.write(back + (fmt % (done,n)))
-    sys.stdout.flush()
-    for i in range(j+1,n_seqs):
-        if labels[i] != labels[j]:
-            continue
-
-        x1,y1 = dimred[i,:]
-        x2,y2 = dimred[j,:]
-
-        if math.sqrt((x2-x1)**2 + (y2-y1)**2) > 0.3:
-            continue
-
-        r1,g1,b1,_    = cm(float(i)/n_seqs)
-        r2,g2,b2,_    = cm(float(j)/n_seqs)
-
-        w1 = ent[i]/(80./9.) + 0.1
-        w2 = ent[j]/(80./9.) + 0.1
-
-        gr = cairo.LinearGradient(x1,y1,x2,y2)
-        gr.add_color_stop_rgba(0.0, r1, g1, b1, w1)
-        gr.add_color_stop_rgba(1.0, r2, g2, b2, w2)
-
-        ctx.set_source(gr)
-
-        ctx.move_to(x1,y1)
-        ctx.line_to(x2,y2)
-        ctx.stroke()
-
-NODE_SIZE = 0.005
-
-print "\nDrawing nodes..."
-
-l = len(str(n_seqs))
-fmt = '%'+str(l)+'d / %'+str(l)+'d'
-back = '\b' * (2*l+3)
-
-sys.stdout.write('\t' + (fmt % (0,n_seqs)))
-
+print "digraph g {"
+print "root [shape=none];"
 for i in range(n_seqs):
-    sys.stdout.write(back + (fmt % (i, n_seqs)))
-    sys.stdout.flush()
-    x,y = dimred[i,:]
-    w = ent[i]/(80./9.) + 0.1
-    ctx.arc(x,y,NODE_SIZE*(0.5+ent[i]*0.5/8.),0, 2 * math.pi)
-    r,g,b,_ = cm(float(i)/n_seqs)
-    ctx.set_source_rgba(r,g,b, w)
-    ctx.fill()
+    print "n%04d [shape=none, label=\"%03x\"];" % (i,i)
 
-print "\nSaving..."
-surface.write_to_png(sys.argv[2])
+for i,j in edges:
+    d = 10.**A[i,j] / 10.
+    print "n%04d -> n%04d [len=\"%f\"];" % (i,j,d)
 
-print "Done."
+i,j = mapping[0], mapping[1]
+print "root -> n%04d [len=\"%f\"];" % (i, A[i,j]/2.)
+print "root -> n%04d [len=\"%f\"];" % (j, A[i,j]/2.)
 
+print "}"
